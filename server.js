3a502ae3c6235f1a6d74595a58add6fe4812af95
Nathan Dezan <dezannathan@gmail.com>
2024-01-09 18:44:59 -0400

    fix: createClient

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const ManageClient  = require('./ManageClient.js');
 require('dotenv').config()
 
-
 const manageClient = new ManageClient(process.env.NO_GUI_SYSTEM, process.env.NO_ROOT_PRIVILEGES);
-client_instance = manageClient.defineClient();
+client_instance = manageClient.createClient();
 
 manageClient.authClient(client_instance);
 manageClient.statusClient(client_instance, 'ready');

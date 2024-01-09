59877e4c7683a6baa42968907c1b50b6ce91734e
Nathan Dezan <dezannathan@gmail.com>
2024-01-09 18:43:02 -0400

    fix: instance ManageClient

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 
 
 const manageClient = new ManageClient(process.env.NO_GUI_SYSTEM, process.env.NO_ROOT_PRIVILEGES);
-client_instance = ManageClient.defineClient();
+client_instance = manageClient.defineClient();
 
-ManageClient.authClient(client_instance);
-ManageClient.statusClient(client_instance, 'ready');
-ManageClient.statusClient(client_instance, 'initialize');
+manageClient.authClient(client_instance);
+manageClient.statusClient(client_instance, 'ready');
+manageClient.statusClient(client_instance, 'initialize');
 
 client_instance.on('message', message => {
 	if(message.body === '!ping') {

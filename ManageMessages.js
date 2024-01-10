71da75b82bf56caff403973610314f179c6f2b47
Nathan Dezan <dezannathan@gmail.com>
2024-01-09 20:24:42 -0400

    fix: ManageMessages (getChat)

--- ManageMessages.js
+++ ManageMessages.js
@@ -6,7 +6,8 @@ class ManageMessages{
     loadAllMessages(){
         this.client_instance.on('message', async message => {
             if(message.body === '!load_all_messages') {
-                const allMessages = await this.client_instance.fetchMessages({limit: 100});
+                const chat = await message.getChat();
+                const allMessages = await chat.fetchMessages({limit: 100});
                 console.log(allMessages);
             }
         });
